9db47101f3dfafcb0e5d13201888b1e60635c697
Elton Okawa <elton.okawa@gmail.com>
2025-01-26 06:49:46 -0300

    refactor: remove unused functions

--- src/app/menu/menu-trigger-harness.component.ts
+++ src/app/menu/menu-trigger-harness.component.ts
@@ -8,16 +8,6 @@ import { MenuHarness } from './menu-harness.component';
 export class MenuTriggerHarness extends ComponentHarness {
   static hostSelector = '.cdk-menu-trigger';
 
-  async getHarnessLoaderForContent(): Promise<HarnessLoader> {
-    const rootLocator = this.documentRootLocatorFactory();
-    return rootLocator.harnessLoaderFor('.cdk-menu');
-  }
-
-  async getContent() {
-    const loader = await this.getHarnessLoaderForContent();
-    return loader.getHarness(MenuHarness);
-  }
-
   async toggle() {
     const trigger = await this.host();
     return trigger.click();
